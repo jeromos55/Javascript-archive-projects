f51c4671cc2c47f77538cfbc9b64b9b4ff9cde5f
jeromos55 <jeromos55@gmail.com>
2024-02-09 14:11:14 +0100

    update

--- Advanced-Bankist-Page/script.js
+++ Advanced-Bankist-Page/script.js
@@ -7,6 +7,16 @@ const account1 = {
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-07-26T17:01:17.194Z',
+    '2020-07-28T23:36:17.929Z',
+    '2020-08-01T10:51:36.790Z',
+  ],
 };
 
 const account2 = {
@@ -14,6 +24,16 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-07-26T17:01:17.194Z',
+    '2020-07-28T23:36:17.929Z',
+    '2020-08-01T10:51:36.790Z',
+  ],
 };
 
 const account3 = {
@@ -21,6 +41,16 @@ const account3 = {
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
 };
 
 const account4 = {
@@ -28,6 +58,16 @@ const account4 = {
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -62,19 +102,31 @@ const inputClosePin = document.querySelector('.form__input--pin');
 //---------------------------------------------
 // BANKIST APP
 
-const displayMovements = function (movements, sort = false) {
+const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
 
-  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+  const movs = sort
+    ? acc.movements.slice().sort((a, b) => a - b)
+    : acc.movements;
 
   movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const isEven = i % 2 === 0 ? 'background-color:rgb(245,245,245);' : '';
+
+    const date = new Date(acc.movementsDates[i]);
+    const now = new Date();
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    const year = now.getFullYear();
+
+    const displayDate = `${day}/${month}/${year}`;
 
     const html = `
-    <div class="movements__row">
+    <div class="movements__row"  style=${isEven}>
       <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
+      <div class="movements__date">${displayDate}</div>
       <div class="movements__value">${mov.toFixed(2)}€</div>
     </div>
     `;
@@ -121,7 +173,7 @@ const calcDisplaySummary = function (acc) {
 
 const updateUI = function (acc) {
   // display movements
-  displayMovements(acc.movements);
+  displayMovements(acc);
 
   // display balance
   calcPrintBalance(acc);
@@ -143,6 +195,15 @@ btnLogin.addEventListener('click', function (e) {
       currentAccount.owner.split(' ')[0]
     }`;
     containerApp.style.opacity = 100;
+
+    const now = new Date();
+    const day = `${now.getDate()}`.padStart(2, 0);
+    const month = `${now.getMonth() + 1}`.padStart(2, 0);
+    const year = now.getFullYear();
+    const hour = `${now.getHours()}`.padStart(2, 0);
+    const min = `${now.getMinutes()}`.padStart(2, 0);
+
+    labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
 
     // clear input field
     inputLoginUsername.value = inputLoginPin.value = '';
@@ -168,6 +229,9 @@ btnTransfer.addEventListener('click', function (e) {
     currentAccount.movements.push(-amount);
     receiverAcc.movements.push(amount);
 
+    currentAccount.movementsDates.push(new Date().toISOString());
+    receiverAcc.movementsDates.push(new Date().toISOString());
+
     updateUI(currentAccount);
   }
 });
@@ -181,6 +245,7 @@ btnLoan.addEventListener('click', function (e) {
     currentAccount.movements.some((mov) => mov >= amount / 10)
   ) {
     currentAccount.movements.push(amount);
+    currentAccount.movementsDates.push(new Date().toISOString());
     updateUI(currentAccount);
   }
   inputLoanAmount.value = '';
--- Beginner-Number-Dates-Times/script.js
+++ Beginner-Number-Dates-Times/script.js
@@ -91,3 +91,113 @@ console.log((2.7).toFixed(0));
 console.log((2.7).toFixed(3));
 console.log((2.345).toFixed(2));
 console.log(+(2.345).toFixed(2));
+
+// remainder operator
+console.log('--- remainder operator ---');
+
+console.log(5 % 2); // 2+2+1 = 5
+console.log(5 / 2);
+
+console.log(8 % 3); // 3+3+2 = 8
+console.log(8 / 3);
+
+console.log(6 % 2); // 2+2+2 = 6
+console.log(6 / 2);
+
+console.log(7 % 2); // 2+2++2+1 = 7
+console.log(7 / 2);
+
+const isEven = (n) => (n % 2 === 0 ? 'even' : 'odd');
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
+
+// numeric separator
+console.log('--- numeric separator ---');
+
+// 286,460.000.000
+const diameter = 286_460_00_000;
+console.log(diameter);
+
+const price = 34_99;
+console.log(price);
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+
+const PI = Math.PI;
+console.log(PI);
+
+console.log(Number(230_000));
+console.log(parseInt('230_000'));
+
+// working with BigInt
+console.log('--- working with BigInt ---');
+
+console.log(2 ** 53 - 1);
+console.log(Number.MAX_SAFE_INTEGER);
+
+// don't correct numbers
+console.log("--- don't correct numbers ---");
+console.log(2 ** 53 + 0);
+console.log(2 ** 53 + 1);
+console.log(2 ** 53 + 2);
+console.log(2 ** 53 + 3);
+
+console.log(567892365348756398376776767674623565789n);
+console.log(BigInt(567892365348756398376776767674623565789));
+console.log(567892365348756398n);
+
+// operation
+console.log(1000000n * 1000000n);
+console.log(458759535384579587498375395757n * 10000000442n);
+// console.log(Math.sqrt(16n)); // TypeError: cannot convert a BigInt value to a number
+
+const huge = 5553455345345n;
+const num = 23;
+// console.log(huge * num); // TypeError: cannot mix BigInt other types
+console.log(huge * BigInt(num));
+
+// exceptions
+console.log(20n > 15);
+console.log(20n === 20);
+console.log(typeof 20n);
+console.log(20n == '20');
+
+// division
+console.log(11n / 3n); // only integer part of the division
+console.log(101 / 3);
+
+// creating dates
+const now = new Date();
+console.log(now);
+
+console.log(new Date('Aug 02 2020 18:05:41'));
+console.log(new Date('December 24, 2015'));
+// console.log(new Date(account1.movementsDates[0]));
+
+console.log(new Date(2037, 10, 19, 15, 23, 5));
+console.log(new Date(2037, 10, 31));
+
+console.log(new Date(0));
+console.log(new Date(3 * 24 * 60 * 60 * 1000));
+
+// Working with dates
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.getTime());
+
+console.log(new Date(2142256980000));
+
+console.log(Date.now());
+
+future.setFullYear(2040);
+console.log(future);
