6d2cad3cab42d62d0f123833c9203b0f77267cdb
jeromos55 <jeromos55@gmail.com>
2024-05-19 14:54:14 +0200

    update

--- Beginner-Examples-15/script.js
+++ Beginner-Examples-15/script.js
@@ -44,9 +44,39 @@ Test data:
 GOOD LUCK 
 */
 
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
 var requestOptions = {
   method: 'GET',
 };
+
+//------------------------------------------------------------
+
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flags.png}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name.common}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)}M people</p>
+      <p class="country__row"><span>🗣️</span>${
+        data.languages[Object.keys(data.languages)[0]]
+      }</p>
+      <p class="country__row"><span>💰</span>${
+        data.currencies[Object.keys(data.currencies)[0]].name
+      }</p>
+    </div>
+  </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+//-------------------------------------------------------------
 
 const whereAmI = function (lat, lng) {
   fetch(
@@ -59,12 +89,20 @@ const whereAmI = function (lat, lng) {
       return response.json();
     })
     .then((res) => {
-      // console.log(result);
       console.log(
         `You are in ${res.features[0].properties.city}, ${res.features[0].properties.country}`
       );
+      return fetch(
+        `https://restcountries.com/v3.1/name/${res.features[0].properties.country}`
+      );
     })
-    .catch((err) => console.error(`${err.message} 💥`));
+    .then((response) => {
+      if (!response.ok)
+        throw new Error(`Country not found (${response.status})`);
+      return response.json();
+    })
+    .then((data) => renderCountry(data[0]))
+    .catch((err) => console.error(`${err.message}`));
 };
 
 whereAmI(52.508, 13.381);
